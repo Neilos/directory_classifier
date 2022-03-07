4c0cf0f94900d2dc4baa23ae07f5a23c5b3efcd4
Neilos <neil.atkinson@smartpension.co.uk>
2022-03-07 14:42:07 +0000

    ContributionSet calculates relative path based on current directory

--- lib/contribution_set.rb
+++ lib/contribution_set.rb
@@ -6,9 +6,6 @@ class ContributionSet
   PathMismatchError = Class.new(StandardError)
   InvalidPathError = Class.new(StandardError)
 
-  RELATIVE_BASE_PATH = Pathname.new('.').cleanpath
-  ABSOLUTE_BASE_PATH = RELATIVE_BASE_PATH.realdirpath
-
   def initialize(path:, contributors: [])
     @pathname = convert_to_relative_pathname(Pathname.new(path))
 
@@ -97,7 +94,7 @@ class ContributionSet
       return File.join(partial_path_segments) if partial_path_segments == other_set_partial_path_segments
     end
 
-    RELATIVE_BASE_PATH.to_path
+    relative_base_pathname.to_path
   end
 
   def for_parent_directory?(other_contribution)
@@ -109,12 +106,23 @@ class ContributionSet
   end
 
   def ensure_real_path!
-    pathname.realdirpath
+    pathname.realpath
   rescue Errno::ENOENT
     raise InvalidPathError
   end
 
   def convert_to_relative_pathname(some_pathname)
-    some_pathname.cleanpath.relative_path_from(some_pathname.relative? ? RELATIVE_BASE_PATH : ABSOLUTE_BASE_PATH)
+    some_pathname.cleanpath.relative_path_from(
+      some_pathname.relative? ? relative_base_pathname : absolute_base_pathname
+    )
   end
+
+  def relative_base_pathname
+    @relative_base_pathname ||= Pathname.new(absolute_base_pathname.to_path).relative_path_from(absolute_base_pathname)
+  end
+
+  def absolute_base_pathname
+    @absolute_base_pathname ||= Pathname.new(FileUtils.pwd)
+  end
+
 end
